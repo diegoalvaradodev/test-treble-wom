c05e29f899e9a2feb505012b7db609e58d264e87
diegoalvaradodev <121699618+diegoalvaradodev@users.noreply.github.com>
2024-06-13 16:46:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,23 @@ app.post('/webhook', (req, res) => {
 });
 
 // Ruta para el webhook de Treble
+// Ruta para el webhook de Treble
 app.post('/treble-webhook', (req, res) => {
   // Procesa la petición desde Treble
   const data = req.body;
-  console.log(data);
-  
+  console.log('Datos recibidos del webhook:', data);
+
   const sessionId = data.session_id; // Obtener el ID de la sesión
+  if (!sessionId) {
+    console.error('session_id no encontrado en la petición');
+    return res.status(400).json({
+      status: 'error',
+      message: 'session_id no encontrado en la petición'
+    });
+  }
+
   const apiUrl = `https://main.treble.ai/session/${sessionId}/update`; // Construir la URL de la API
   // Preparar los datos de la petición
- console.log("El API a ejecutar es: "+apiUrl);
   const apiData = {
     user_session_keys: [
       {
@@ -35,22 +43,25 @@ app.post('/treble-webhook', (req, res) => {
       },
       {
         key: "valor",
-        value: "$69.690"
+        value: "$39.990"
       }
     ]
   };
 
-
-// Headers de autenticación
+  // Headers de autenticación
   const headers = {
-    'Authorization': 'ak_Y66T0p94jjd790p4I1TKY9IdszOKr_X8Kw', // Reemplaza YOUR_ACCESS_TOKEN con tu token de autenticación real
+    'Authorization': 'Bearer ak_Y66T0p94jjd790p4I1TKY9IdszOKr_X8Kw', // Reemplaza YOUR_ACCESS_TOKEN con tu token de autenticación real
     'Content-Type': 'application/json'
   };
-    
+
+  console.log('Iniciando peticion Axios POST a la API:', apiUrl);
+  console.log('Datos de la petición:', apiData);
+  console.log('Headers de la petición:', headers);
+
   // Realizar la petición a la API de Treble
   axios.post(apiUrl, apiData, { headers })
     .then(response => {
-      console.log('Response data:', response.data);
+      console.log('Response Axios data:', response.data);
 
       // Envía una respuesta a Treble
       res.json({
@@ -63,11 +74,12 @@ app.post('/treble-webhook', (req, res) => {
           },
           {
             key: 'valor',
-            value: '$59.990'
+            value: '$39.990'
           }
         ]
       });
-    })
+    });
+    console.log('Final respuesta json:', res.json);
     .catch(error => {
       console.error('Error making POST request:', error);
 
