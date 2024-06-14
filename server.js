45aa26f0aa1defccedb34b749ca089123f14138c
diegoalvaradodev <121699618+diegoalvaradodev@users.noreply.github.com>
2024-06-14 10:27:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -75,67 +75,78 @@ app.post('/webhook', (req, res) => {
 
 // Ruta para el webhook de Treble
 app.post('/treble-webhook', (req, res) => {
-    const sessionId = res.session_id; // Asegúrate de que sessionId esté definido correctamente
-    const apiUrl = `https://main.treble.ai/session/${sessionId}/update`; // Construir la URL de la API
-  
-    // Preparar los datos de la petición
-    const apiData = {
-        user_session_keys: [
+  // Procesa la petición desde Treble
+  const data = req.body;
+  console.log('Datos recibidos del webhook:', data);
+
+  const sessionId = data.session_id; // Obtener el ID de la sesión
+  if (!sessionId) {
+    console.error('session_id no encontrado en la petición');
+    return res.status(400).json({
+      status: 'error',
+      message: 'session_id no encontrado en la petición'
+    });
+  }
+
+  const apiUrl = `https://main.treble.ai/session/${sessionId}/update`; // Construir la URL de la API
+  // Preparar los datos de la petición
+  const apiData = {
+    user_session_keys: [
+      {
+        key: "tipo_plan",
+        value: "pospago"
+      },
+      {
+        key: "valor",
+        value: "$29.990"
+      }
+    ]
+  };
+
+  // Headers de autenticación
+  const headers = {
+    'Authorization': 'Bearer ak_Y66T0p94jjd790p4I1TKY9IdszOKr_X8Kw', // Reemplaza YOUR_ACCESS_TOKEN con tu token de autenticación real
+    'Content-Type': 'application/json'
+  };
+
+  console.log('Iniciando peticion Axios POST a la API en 3 segundos:', apiUrl);
+  console.log('Datos de la petición:', apiData);
+  console.log('Headers de la petición:', headers);
+
+  // Agregar tiempo de espera de 3 segundos
+  setTimeout(() => {
+    // Realizar la petición a la API de Treble
+    axios.post(apiUrl, apiData, { headers })
+      .then(response => {
+        console.log('Response Treble data:', response.data);
+          console.log('Response Treble status:', response.status);
+        // Envía una respuesta a Treble
+        res.json({
+          status: 'success',
+          message: 'Petición recibida correctamente y POST request realizada',
+          user_session_keys: [
             {
-                key: "tipo_plan",
-                value: "pospago"
+              key: 'tipo_plan',
+              value: 'pospago'
             },
             {
-                key: "valor",
-                value: "$29.990"
+              key: 'valor',
+              value: '$39.990'
             }
-        ]
-    };
+          ]
+        });
+      })
+      .catch(error => {
+        console.error('Error making POST request:', error);
 
-    // Headers de autenticación
-    const headers = {
-        'Authorization': 'ak_Y66T0p94jjd790p4I1TKY9IdszOKr_X8Kw', // Reemplaza YOUR_ACCESS_TOKEN con tu token de autenticación real
-        'Content-Type': 'application/json'
-    };
-
-    console.log('Iniciando peticion Axios POST a la API en 3 segundos:', apiUrl);
-    console.log('Datos de la petición:', apiData);
-    console.log('Headers de la petición:', headers);
-
-    // Agregar tiempo de espera de 3 segundos
-    setTimeout(() => {
-        // Realizar la petición a la API de Treble
-        axios.post(apiUrl, apiData, {headers})
-            .then(response => {
-                console.log('Response Treble data:', response.data);
-                console.log('Response Treble status:', response.status);
-                // Envía una respuesta a Treble
-                res.json({
-                    status: 'success',
-                    message: 'Petición recibida correctamente y POST request realizada',
-                    user_session_keys: [
-                        {
-                            key: 'tipo_plan',
-                            value: 'pospago'
-                        },
-                        {
-                            key: 'valor',
-                            value: '$39.990'
-                        }
-                    ]
-                });
-            })
-            .catch(error => {
-                console.error('Error making POST request:', error);
-
-                // Envía una respuesta a Treble con el error
-                res.status(500).json({
-                    status: 'error',
-                    message: 'Error processing webhook',
-                    error: error.message
-                });
-            });
-    }, 2000); // 2 segundos
+        // Envía una respuesta a Treble con el error
+        res.status(500).json({
+          status: 'error',
+          message: 'Error processing webhook',
+          error: error.message
+        });
+      });
+  }, 3000); // 3000 milisegundos = 3 segundos
 });
 
 // Configurar el puerto
