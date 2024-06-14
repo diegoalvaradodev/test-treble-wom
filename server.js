ed90f200bdac5aedff94f3a451307100f37c8305
diegoalvaradodev <121699618+diegoalvaradodev@users.noreply.github.com>
2024-06-14 10:01:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,65 @@ app.get('/test', (req, res) => {
 
 // Ruta para el webhook
 app.post('/webhook', (req, res) => {
-    console.log('Webhook received:', req.body);
-    res.status(200).send('Webhook received');
+    const apiUrl = `https://main.treble.ai/session/${sessionId}/update`; // Construir la URL de la API
+  // Preparar los datos de la petición
+  const apiData = {
+    user_session_keys: [
+      {
+        key: "tipo_plan",
+        value: "pospago"
+      },
+      {
+        key: "valor",
+        value: "$29.990"
+      }
+    ]
+  };
+
+  // Headers de autenticación
+  const headers = {
+    'Authorization': 'Bearer ak_Y66T0p94jjd790p4I1TKY9IdszOKr_X8Kw', // Reemplaza YOUR_ACCESS_TOKEN con tu token de autenticación real
+    'Content-Type': 'application/json'
+  };
+
+  console.log('Iniciando peticion Axios POST a la API:', apiUrl);
+  console.log('Datos de la petición:', apiData);
+  console.log('Headers de la petición:', headers);
+
+  // Realizar la petición a la API de Treble
+  axios.post(apiUrl, apiData, {headers})
+    .then(response => {
+      console.log('Response Axios data:', response.data);
+         // Envía una respuesta a Treble
+      res.json({
+        status: 'success',
+        message: 'Petición recibida correctamente y POST request realizada',
+        user_session_keys: [
+          {
+            key: 'tipo_plan',
+            value: 'pospago'
+          },
+          {
+            key: 'valor',
+            value: '$39.990'
+          }
+        ]
+      });
+    })
+    .catch(error => {
+      console.error('Error making POST request:', error);
+
+      // Envía una respuesta a Treble con el error
+      res.status(500).json({
+        status: 'error',
+        message: 'Error processing webhook',
+        error: error.message
+      });
+    });
 });
 
-// Ruta para el webhook de Treble
+
+
 // Ruta para el webhook de Treble
 app.post('/treble-webhook', (req, res) => {
   // Procesa la petición desde Treble
