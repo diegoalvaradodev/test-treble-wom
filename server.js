cbbf69b6198b7bd72b35ed8f135ce4d9ef645c70
diegoalvaradodev <121699618+diegoalvaradodev@users.noreply.github.com>
2024-06-21 19:24:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,16 +23,49 @@ app.get('/test', (req, res) => {
 app.post('/treble-webhook-dos', (req, res) => {
     const data = req.body;
     console.log('Datos recibidos del webhook dos:', data); 
-    res.json({
-        status: 'success',
-        message: 'Petición recibida correctamente y POST request realizada',
-         user_session_keys: [
-            {
-              key: 'estado_portabilidad',
-              value: 'exitosa'
-            }
-          ]
-    });
+    const apiUrl = `https://whatsappportabilidad.middlewom.co:443/requestnipcode/api/v1/requestnipcode/?user_key=588ff92551a096734491602cdda23a10`; // Construir la URL de la API
+      // Preparar los datos de la petición
+      const apiData = {    
+        "init": {
+            "isRedirect": "JSON",
+            "query": "REQUEST_NIP_CODE"
+        },
+        "body":{
+            "origin": "what_port",
+            "serviceNumber": "573137527749",
+            "donorCode": "3",
+            "requestType": "1"
+        }
+      };
+    
+      console.log('Iniciando peticion NIP a Treble:', apiUrl);
+      console.log('Datos de la petición NIP:', apiData);
+    
+    // Realizar la petición a la API de Treble para actualizar chat
+        axios.post(apiUrl, apiData)
+          .then(response => {
+            console.log('Response NIP:', response);
+        })
+        .catch(error => {
+            console.error('Error making POST request:', error);
+            // Envía una respuesta a Treble con el error
+            res.status(500).json({
+                status: 'error',
+                message: 'Error processing webhook',
+                error: error.message
+            });
+        });
+    
+        res.json({
+            status: 'success',
+            message: 'Petición recibida correctamente y POST request realizada',
+             user_session_keys: [
+                {
+                  key: 'estado_portabilidad',
+                  value: 'exitosa'
+                }
+              ]
+        });
 });
 
 
